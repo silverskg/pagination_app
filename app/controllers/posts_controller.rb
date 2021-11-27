97aac255b43853b3b49fba548fee4788846e8fcd
silverskg <chokichoki_gm@i.softbank.jp>
2021-11-27 16:58:54 +0900

    posts form 作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
   def index
+    posts = Post.all
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
