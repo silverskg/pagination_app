01475d03b5459f1aa44d6a58a093809d5693ecd4
silverskg <chokichoki_gm@i.softbank.jp>
2021-11-27 18:52:49 +0900

    新しい投稿が上にくるように order(created_at: :desc)追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.paginate(page: params[:page], per_page: 10)
+    @posts = Post.paginate(page: params[:page], per_page: 10).order(created_at: :desc)
   end
 
   def create
